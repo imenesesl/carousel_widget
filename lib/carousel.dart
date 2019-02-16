83c45b6bf52fce6f6080f55ddd0d9a6e724788c3
imenesesl <luismenesesep@gmail.com>
2019-02-16 16:15:48 -0500

    edit structure and export

--- lib/carousel.dart
+++ lib/carousel.dart
@@ -1,19 +1,4 @@
-import 'package:carousel_widget/fragment.dart';
-import 'package:carousel_widget/widgets/carousel/carousel_controller.dart';
-import 'package:flutter/material.dart';
+library material;
 
-class Carousel extends StatelessWidget {
-  final List<Fragment> listViews;
-  Carousel({
-    @required this.listViews,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      bottom: false,
-      child: CarouselController(
-        listViews: listViews,
-      ),
-    );
-  }
-}
+export 'src/carousel/widgets/carousel.dart';
+export 'src/carousel/widgets/fragment.dart';
